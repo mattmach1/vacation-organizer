4fb306d9f99157687fee1fd831dddf50c8acf2fb
Matt <mbrown2595@gmail.com>
2025-03-23 12:54:26 -0500

    created Excusion entity

---
+++ d308mobileapplication/app/src/main/java/com/example/d308mobileapplication/database/VacationDatabaseBuilder.java
@@ -0,0 +1,4 @@
+package com.example.d308mobileapplication.database;
+
+public class VacationDatabaseBuilder {
+}
--- d308mobileapplication/app/src/main/java/com/example/d308mobileapplication/entities/Excursion.java
+++ d308mobileapplication/app/src/main/java/com/example/d308mobileapplication/entities/Excursion.java
@@ -1,4 +1,42 @@
 package com.example.d308mobileapplication.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "excursions")
 public class Excursion {
+    @PrimaryKey(autoGenerate = true)
+    private int excursionID;
+    private String excursionTitle;
+    private String excursionDate;
+
+    public int getExcursionID() {
+        return excursionID;
+    }
+
+    public void setExcursionID(int excursionID) {
+        this.excursionID = excursionID;
+    }
+
+    public String getExcursionTitle() {
+        return excursionTitle;
+    }
+
+    public void setExcursionTitle(String excursionTitle) {
+        this.excursionTitle = excursionTitle;
+    }
+
+    public String getExcursionDate() {
+        return excursionDate;
+    }
+
+    public void setExcursionDate(String excursionDate) {
+        this.excursionDate = excursionDate;
+    }
+
+    public Excursion(int excursionID, String excursionTitle, String excursionDate) {
+        this.excursionID = excursionID;
+        this.excursionTitle = excursionTitle;
+        this.excursionDate = excursionDate;
+    }
 }
